ad9993b5c3229f63db4a3f94d5cf6abdc927d666
zhengbrayden <zhengbrayden@gmail.com>
2025-03-10 13:43:10 -0400

    add in memory storage code

--- room/storage.py
+++ room/storage.py
@@ -1,3 +1,15 @@
 from .logic.channel import Channel
 
+class slugGenerator():
+    def __init__(self):
+        self.name2slug = {}
+        self.slug2name = {}
+        self.slugcount = -1
+    def create_slug(self, name):
+        self.slugcount += 1
+        self.name2slug[name] = self.slugcount
+        self.slug2name[str(self.slugcount)] = name
+        return self.slugcount
+
+slug_generator = slugGenerator()
 channel = Channel()
